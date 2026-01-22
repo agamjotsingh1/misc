a74e011668e8fa1827100a82e8607928c77f27cf
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2026-01-23 00:48:40 +0530

    real estate done

---
+++ stats/real_estate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+BINS = 50
+
+df = pd.read_csv("datasets/real_estate.csv")
+df = df.dropna(subset=["price_in_USD"])
+
+prices = df["price_in_USD"]
+
+# exclude prices more than 1.5 million
+# these are extreme outliers
+prices = prices[prices <= 1.5e6]
+
+# FREQUENCY HISTOGRAM
+plt.figure(figsize=(16, 6))
+prices.hist(bins=BINS, edgecolor='black')
+plt.title("Frequency Histogram for Price (in USD) of Real Estate in the United States")
+plt.xlabel("Price (in USD)")
+plt.ylabel("Real Estate Frequency")
+plt.savefig("plots/real_estate_frequency_histogram.png")
+plt.show()
+
+# RELATIVE FREQUENCY HISTOGRAM
+plt.figure(figsize=(16, 6))
+prices.hist(bins=BINS,
+            edgecolor='black',
+            weights=[1/len(prices)]*len(prices),
+            color="orange")
+plt.title("Relative frequency Histogram for Price (in USD) of Real Estate in the United States")
+plt.xlabel("Price (in USD)")
+plt.ylabel("Real estate relative frequency")
+plt.savefig("plots/real_estate_relative_frequency_histogram.png")
+plt.show()
