5204743e4b41960f11f74c78dc64f8e16c4bcaf5
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2026-01-23 01:22:58 +0530

    all plots done

---
+++ stats/gdp.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("datasets/gdp.csv")
+df = df.dropna(subset=["2024"])
+
+pollutants = df["2024"]
+
+# FREQUENCY HISTOGRAM
+plt.figure(figsize=(10, 6))
+plt.boxplot(pollutants, orientation="horizontal")
+plt.title("Box plot of GDP per capita (in USD$) of different countries")
+plt.xlabel("GDP per capita (in USD$)")
+plt.savefig("plots/gdp_box_plot.png")
+plt.show()
